5c45c8dd18e89b0e910c2b963c51a1d66b9caba2
Boris Ermakov-Spektor <boris.ermakov-spektor@ukg.com>
2021-01-07 09:44:27 -0500

    Trying to setup websocket

--- src/main/java/com/techstars/whiteboard/WhiteboardApplication.java
+++ src/main/java/com/techstars/whiteboard/WhiteboardApplication.java
@@ -2,6 +2,10 @@ package com.techstars.whiteboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class WhiteboardApplication {
@@ -10,4 +14,13 @@ public class WhiteboardApplication {
         SpringApplication.run(WhiteboardApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowCredentials(true).allowedOrigins("*").allowedMethods("*");
+            }
+        };
+    }
 }
